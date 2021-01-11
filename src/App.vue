fa89e73952c6a4f8a0e137255672e6f660f11b5b
iofuwisa <ootuka70@gmail.com>
2021-01-11 15:18:02 +0900

    First commit Randomsite_generator

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Random site Generator</h1>
+    <Randomsite/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Randomsite from './components/Randomsite.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Randomsite
   }
 }
 </script>
---
+++ src/components/Randomsite.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <a :href="url">{{url}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Randomsite',
+  data: () => ({
+    url: ""
+  }),
+
+  mounted: function(){
+    const axiosBase = require('axios')
+    const axios = axiosBase.create({
+        baseURL: 'https://8s01jod446.execute-api.ap-northeast-1.amazonaws.com/site_crawler_api_stage',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        responseType: 'json'  
+    });
+
+    axios.get('/')
+      .then(response => {
+        console.log(response.data.body['url'])
+        this.url = response.data.body['url']
+      })
+      .catch(response => console.log(response))
+
+  } 
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
