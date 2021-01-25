8e429c6f8b88e5b2049c9131b314e464d2981229
iofuwisa <ootuka70@gmail.com>
2021-01-25 22:51:33 +0900

    Google analitycs.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueAnalytics from 'vue-analytics';
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -16,6 +17,11 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+Vue.use(VueAnalytics, {
+  id: 'G-964MQ92E0M',
+  router
 })
 
 export default router
